1666e215c12d3e1f931d4d88c67d529372ef8467
cevin <qixingyue@gmail.com>
2025-07-06 09:51:27 +0800

    fast: pyhton bug

--- py-client/main.py
+++ py-client/main.py
@@ -3,6 +3,8 @@ import ctypes
 
 # 加载 Rust 库
 rust_lib = ctypes.CDLL('../target/release/librust_so_example.dylib')
+rust_lib.get_pkg_version.restype = ctypes.c_char_p
+rust_lib.get_pkg_name.restype = ctypes.c_char_p
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -16,7 +18,7 @@ def main():
     logger.info(f"SO Name: {so_name}")
     
     so_version = rust_lib.get_pkg_version()
-    logger.info(f"SO Version: {so_version}")
+    logger.info(f"SO Version: {ctypes.cast(so_version, ctypes.c_char_p).value.decode('utf-8')}")
 
 if __name__ == "__main__":
     main()
