d9216143fa849fae9f2d79cfa9377c75a88f1999
cevin <qixingyue@gmail.com>
2025-07-06 09:36:48 +0800

    py so client

---
+++ py-client/main.py
@@ -0,0 +1,22 @@
+import logging
+import ctypes
+
+# 加载 Rust 库
+rust_lib = ctypes.CDLL('../target/release/librust_so_example.dylib')
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def main():
+    logger.info("Starting the application")
+    result = rust_lib.add(b"42aa", b"235")
+    logger.info(f"Result: {result}")
+    
+    so_name = rust_lib.get_pkg_name()
+    logger.info(f"SO Name: {so_name}")
+    
+    so_version = rust_lib.get_pkg_version()
+    logger.info(f"SO Version: {so_version}")
+
+if __name__ == "__main__":
+    main()
